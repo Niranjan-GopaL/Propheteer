967057ba476f076f2e6550ff9b866991d78b9dab
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-04-27 16:55:19 +0530

    -Configures Tailwind -Made file and folder structure and started working on Navbar.

---
+++ app/post/[id]/loading.tsx
@@ -0,0 +1,10 @@
+// This is also a reserved keyword. This is used as Loading screen
+import React from "react";
+
+const loading = () => (
+    <>
+    Loading
+    </>
+)
+
+export default loading;
